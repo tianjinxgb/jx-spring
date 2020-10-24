6a0e67e81d8cbe7cf0699ec29c750b177695a574
xing.guobiao <tianjinxgb@126.com>
2020-10-24 18:52:56 +0800

    init

---
+++ src/test/java/com/xgb/test/jxspring/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xgb.test.jxspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
